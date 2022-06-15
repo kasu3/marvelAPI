9c01db84a552dc22d8b90e407a3c73fd9280d7b7
Konstantin <kouta955@gmail.com>
2022-06-15 05:22:08 +0300

    CSSTransotion on charList and comicList

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -26,62 +26,62 @@ const CharList = (props) => {
 			.then(onCharListLoaded);
 	}
 
-	const onCharListLoaded = (newCharList) => {
+	const onCharListLoaded = async(newCharList) => {
 		let ended = false;
 		if(newCharList.length < 9) {
 			ended = true;
 		}
 
-		setCharList(charList => [...charList, ...newCharList]);
-		setNewItemLoading(newItemLoading => false);
-		setOffset(offset => offset + 9);
-		setCharEnded(charEnded => ended);
+		setCharList([...charList, ...newCharList]);
+		setNewItemLoading(false);
+		setOffset(offset + 9);
+		setCharEnded(ended);
 	}
 
 	const itemRefs = useRef([]);
 	
 	const focusOnItem = (id) => {
-		itemRefs.current.forEach(item => item.classList.remove('char__item_selected'));
-		itemRefs.current[id].classList.add('char__item_selected');
-		itemRefs.current[id].focus();
+        itemRefs.current.forEach(item => item.classList.remove('char__item_selected'));
+        itemRefs.current[id].classList.add('char__item_selected');
+        itemRefs.current[id].focus();
 	}
 
 	function renderItems(arr) {
-		const items = arr.map((item, i) => {
-			let imgStyle = {'objectFit' : 'cover'};
-			if(item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-				imgStyle = {'objectFit' : 'unset'};
-			}
-
-			return (
-				<CSSTransition key={item.id} timeout={500} classNames="char__item">
-					<li
-						className="char__item"
-						tabIndex={0}
-						ref={el => itemRefs.current[i] = el}
-						onClick={() => {
-							props.onCharSelected(item.id);
-							focusOnItem(i);
-						}}
-						onKeyPress={(e) => {
-							if (e.key === ' ' || e.key === "Enter") {
-								props.onCharSelected(item.id);
-								focusOnItem(i);
-							}
-						}}>
-							<img src={item.thumbnail} alt={item.name} style={imgStyle}/>
-							<div className="char__name">{item.name}</div>
-					</li>
-				</CSSTransition>
-			)
-		});
+		const items =  arr.map((item, i) => {
+            let imgStyle = {'objectFit' : 'cover'};
+            if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+                imgStyle = {'objectFit' : 'unset'};
+            }
+            
+            return (
+                <CSSTransition key={item.id} timeout={500} classNames="char__item">
+                    <li 
+                        className="char__item"
+                        tabIndex={0}
+                        ref={el => itemRefs.current[i] = el}
+                        onClick={() => {
+                            props.onCharSelected(item.id);
+                            focusOnItem(i);
+                        }}
+                        onKeyPress={(e) => {
+                            if (e.key === ' ' || e.key === "Enter") {
+                                props.onCharSelected(item.id);
+                                focusOnItem(i);
+                            }
+                        }}>
+                            <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
+                            <div className="char__name">{item.name}</div>
+                    </li>
+                </CSSTransition>
+            )
+        });
 
 		return (
-			<ul className="char__grid">
-				<TransitionGroup component={null}>
-					{items}
-				</TransitionGroup>
-			</ul>
+            <ul className="char__grid">
+                <TransitionGroup component={null}>
+                    {items}
+                </TransitionGroup>
+            </ul>
 		)
 	}
     
